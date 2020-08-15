f83edeb64ab10a58d84684a38e4ea420bcc2e1fc
Silelou Bernard Chawo <bernard.silenou@helmholtz-hzi.de>
2020-08-16 01:34:39 +0200

    added xml package

--- requirements.R
+++ requirements.R
@@ -27,6 +27,7 @@ install.packages('EpiEstim', version='2.2-3', repos='http://cran.us.r-project.or
 install.packages('incidence', version='1.7.2', repos='http://cran.us.r-project.org')
 install.packages('tidyr', version='1.1.1', repos='http://cran.us.r-project.org')
 #install.packages('xml2', version='1.3.2', repos='http://cran.us.r-project.org')  # needed fro tmaptools
+remotes::install_version(package = "XML", version = "3.99-0.3")
 devtools::install_github("mtennekes/tmaptools")  #dependency of tmap
 devtools::install_github("r-spatial/sf")  # dependency of tmap 
 devtools::install_github("rspatial/raster")  # dependency of tmap 
