0c6e331b6023fb4cc152a8f945d99da074c97846
“bernardsilenou” <bernard.silenou@helmholtz-hzi.de>
2020-12-01 01:14:15 +0100

    [#88] cleanup function to do export of network diagram data

--- src/stats/R/export/contact_export.R
+++ src/stats/R/export/contact_export.R
@@ -1,9 +1,16 @@
+# This function connects to the sormas db and exports to contact data for contacts with known 
+# source case together with their source case information and jurisdiction. 
+# returned data will later be exported to sormas-stats db
+# as specified by issue https://github.com/hzi-braunschweig/SORMAS-Stats/issues/97
+# Dependandy utils : sormas_db, fixContactJurisdiction
+# Dependancy packages: RPostgreSQL, lubridate, dplyr
+
 contact_export <- function(sormas_db){
   # load cases
   case = dbGetQuery(
     sormas_db,
-    "SELECT uuid AS case_uuid, id AS case_id, person_id AS person_id_case, region_id AS region_id_case, reportdate AS report_date_case,
-    district_id AS district_id_case, caseclassification AS case_classification, symptoms_id, disease AS disease_case
+    "SELECT uuid AS case_uuid, id AS case_id, person_id AS person_id_case, region_id AS region_id_case, 
+    district_id AS district_id_case, caseclassification AS case_classification, disease AS disease_case
     FROM cases
     WHERE deleted = FALSE and caseclassification != 'NO_CASE'"
   )
@@ -12,21 +19,14 @@ contact_export <- function(sormas_db){
   # caze_id IS NOT NULL required b/c returning travelers are mapped as contacts without source case
   contact = dbGetQuery(
     sormas_db,
-    "SELECT uuid AS contact_uuid, id AS contact_id, caze_id AS case_id, district_id AS district_id_contact, disease AS disease_contact, region_id AS region_id_contact,
-    person_id AS person_id_contact, reportdatetime AS report_date_contact, lastcontactdate AS lastcontact_date_contact, 
-    contactproximity AS contact_proximity, resultingcase_id, contactstatus, contactclassification, followupstatus, followupuntil,
-    relationtocase
+    "SELECT uuid AS contact_uuid, id AS contact_id, caze_id AS case_id, district_id AS district_id_contact,
+    disease AS disease_contact, region_id AS region_id_contact, person_id AS person_id_contact, 
+    reportdatetime AS report_date_contact, lastcontactdate AS lastcontact_date_contact, contactproximity AS contact_proximity, 
+    resultingcase_id, contactstatus, contactclassification, followupstatus, relationtocase
     FROM public.contact
     WHERE deleted = FALSE AND caze_id IS NOT NULL" 
   )
-
-  # load symptom data
-  symptoms = dbGetQuery(
-    sormas_db,
-    "SELECT uuid AS symptom_uuid, id AS symptom_id, onsetdate AS onset_date
-    FROM public.symptoms"
-  )
- 
+  
   # load regions
   region = dbGetQuery(
     sormas_db,
@@ -45,26 +45,13 @@ contact_export <- function(sormas_db){
   
   # clean-up tables 
   # fix date formats
-  case = case %>%
-    dplyr::mutate(report_date_case = as.Date(format(report_date_case, "%Y-%m-%d")))
-  
   contact  = contact %>%
     dplyr::mutate(report_date_contact = as.Date(format(report_date_contact, "%Y-%m-%d")),
-           lastcontact_date_contact = as.Date(format(lastcontact_date_contact, "%Y-%m-%d")),
-           followupuntil = as.Date(format(followupuntil, "%Y-%m-%d"))
+           lastcontact_date_contact = as.Date(format(lastcontact_date_contact, "%Y-%m-%d"))
   )
-  
-  symptoms = symptoms %>%
-    dplyr::mutate(onset_date = as.Date(format(onset_date, "%Y-%m-%d")))
-  
-  events = events %>%
-    dplyr::mutate(report_date_event = as.Date(format(report_date_event, "%Y-%m-%d")))
-  
-  # converting id to character for eary stacking of tables
-  person$person_id = as.character(person$person_id)
+  # converting person ids to character for eary stacking of tables with nodes from event
   contact$person_id_contact = as.character(contact$person_id_contact )
   case$person_id_case = as.character(case$person_id_case)
-  eventsParticipant$person_id_eventpart = as.character(eventsParticipant$person_id_eventpart)
 
   # merging data to generate the line-list of contacts
   export_contacts = contact %>%
@@ -72,7 +59,7 @@ contact_export <- function(sormas_db){
     dplyr::left_join(., case, by="case_id") %>%
     # giving contacts with missing region id the region and district id of their source cases
     fixContactJurisdiction(contCase = .) %>%
-    # dropping jurisdiction of source case
+    # dropping jurisdiction id of source case
     dplyr::select(., -c(region_id_case, district_id_case )) %>%
     # merging with region table to get region details of  infector person
     dplyr::left_join(., region, by = c("region_id_contact" = "region_id" )) %>%
@@ -81,7 +68,11 @@ contact_export <- function(sormas_db){
     
     # export_contacts dataset has unique contacts but not unique person to person pair,
     # thus we need to filter it to het unique adges between 2 nodes
-    dplyr::mutate(region_name_contact = region_name, district_name_contact = district_name, case_classification_infector = case_classification, .keep = "unused")
+    dplyr::mutate(region_name_contact = region_name, district_name_contact = district_name, 
+                  case_classification_infector = case_classification, .keep = "unused") %>%
+    # dropping jurisdiction id of contacts
+    dplyr::select(., -c(region_id_contact, district_id_contact ))
+    
     
     return(export_contacts)
 }
