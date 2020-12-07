4992053a4be17d4a0a0bdb6ce303a733c8700afa
Jonas Cirotzki <s8jociro@stud.uni-saarland.de>
2020-12-07 20:44:15 +0100

    [#84] single lib load on docker

--- src/stats/config/run_stats.sh
+++ src/stats/config/run_stats.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
+# This export is needed to make vars available in the env, I really don't know why
+export DB_HOST
+
 echo "BEGIN R"
 pushd /srv/src/R/
 Rscript main.R
 popd
+
 echo "END R"
 echo "BEGIN python"
 python /srv/src/python/main.py
