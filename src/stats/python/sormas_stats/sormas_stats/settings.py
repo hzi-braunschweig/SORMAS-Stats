c1d89f6d3fcdd5d9fb1fca6bd8329a82a1b69a78
Jonas Cirotzki <jonas.cirotzki@flixbus.com>
2021-01-04 16:26:09 +0100

    fix env

--- src/stats/python/sormas_stats/sormas_stats/settings.py
+++ src/stats/python/sormas_stats/sormas_stats/settings.py
@@ -9,24 +9,22 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e3avn)f23qj+5n3p+--3(wxe3!#lyk#bzynsk4k&t#q&1)dddf'
+SECRET_KEY = 'CHANGE-ME!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +67,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sormas_stats.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sormas_stats',
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'USER': os.getenv('SORMAS_STATS_POSTGRES_USER', 'stats_user'),
+        'PASSWORD': os.getenv('SORMAS_POSTGRES_PASSWORD', 'password')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -99,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -112,7 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
