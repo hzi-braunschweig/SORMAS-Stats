26a6660fac44b5760b61cd900943ffd54e4e4504
Jonas Cirotzki <jonas.cirotzki@flixbus.com>
2021-01-04 22:33:19 +0100

    reenable cases per day

--- src/R/main.R
+++ src/R/main.R
@@ -5,7 +5,6 @@ library(RPostgres)
 
 source(here('R/env_setup.R'))
 
-
-# sormas_db <- do_connect('sormas', 'sormas_reader', 'password')
-# stats_db <- do_connect('sormas_stats', 'stats_user', 'password')
-# epi_number_of_cases_per_day(sormas_db)
+sormas_db <- do_connect('sormas', 'sormas_reader', 'password')
+stats_db <- do_connect('sormas_stats', 'stats_user', 'password')
+epi_number_of_cases_per_day(sormas_db)
